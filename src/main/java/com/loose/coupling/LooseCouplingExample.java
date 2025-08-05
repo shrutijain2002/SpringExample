d696e95b5bb1be36931235f3ec9f5ba801f62608
Shruti Jain <smsjain197012@gmail.com>
2025-08-05 10:29:27 +0530

    Introduced Loose Coupling

--- src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -2,7 +2,16 @@ package com.loose.coupling;
 
 public class LooseCouplingExample {
     public static void main(String[] args) {
-        UserManager userManager = new UserManager();
-        System.out.println(userManager.getUserInfo());
+        UserDataProvider databaseProvider = new UserDatabaseProvider();
+        UserManager userManagerWithDB = new UserManager(databaseProvider);
+        System.out.println(userManagerWithDB.getUserInfo());
+
+        UserDataProvider webServiceProvider = new WebServiceDataProvider();
+        UserManager userManagerWithWS = new UserManager(webServiceProvider);
+        System.out.println(userManagerWithWS.getUserInfo());
+
+        UserDataProvider newDatabaseProvider = new NewDatabaseProvider();
+        UserManager userManagerWithNewDB = new UserManager(newDatabaseProvider);
+        System.out.println(userManagerWithNewDB.getUserInfo());
     }
 }
